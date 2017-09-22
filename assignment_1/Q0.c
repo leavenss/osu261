1121b931feb5699eca43d38856608f2852a03408
Taylor Liss <tayloraliss@gmail.com>
2017-09-22 10:27:55 -0400

    Added final comments and changed printf statements

--- assignment_1/Q0.c
+++ assignment_1/Q0.c
@@ -1,7 +1,9 @@
 /* CS261- Assignment 1 - Q. 0*/
 /* Name: Taylor Liss
  * Date: 9/21/17
- * Solution description:
+ * Solution description: This program assigns a value to x, then assigns a pointer to point to that variable. It prints
+ * the value pointed to by the pointer, the address pointed to by the pointer, the address of the pointer itself, and
+ * the value of x.
  */
 
 #include <stdio.h>
@@ -9,11 +11,11 @@
 
 void fooA(int* iptr){
     /*Print the value pointed to by iptr*/
-    printf("%d\n", *iptr);
+    printf("Value pointed to by iptr: %d\n", *iptr);
     /*Print the address pointed to by iptr*/
-    printf("%p\n", iptr);
+    printf("Address pointed to by iptr: %p\n", iptr);
     /*Print the address of iptr itself*/
-    printf("%p\n", &iptr);
+    printf("Address of iptr itself: %p\n", &iptr);
 }
 
 int main(){
@@ -21,10 +23,10 @@ int main(){
     /*declare an integer x*/
     int x = 7;
     /*print the address of x*/
-    printf("%p\n", &x);
+    printf("Address of x: %p\n", &x);
     /*Call fooA() with the address of x*/
     fooA(&x);
     /*print the value of x*/
-    printf("%d\n", x);
+    printf("Value of x: %d\n", x);
     return 0;
 }
