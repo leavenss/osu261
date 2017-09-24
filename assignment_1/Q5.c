25bd3ce0f98ad7b114ac5a442ec84df341750412
Taylor A. Liss <tayloraliss@gmail.com>
2017-09-24 13:37:40 -0400

    Add more code to Q5

--- assignment_1/Q5.c
+++ assignment_1/Q5.c
@@ -19,14 +19,30 @@ char toLowerCase(char ch){
 
 void sticky(char* word){
     /*Convert to sticky caps*/
+    for (int i=0; i<20; i++){
+        if (i % 2 == 1 && word[i] >= 'a' && word[i] <= 'z') {
+            printf("Running toUpper\n");
+            word[i] = toUpperCase(word[i]);
+        } else if (i % 2 == 0 && word[i] >= 'A' && word[i] <= 'Z'){
+            printf("Running toLower\n");
+            word[i] = toLowerCase(word[i]);
+        } else if (i % 2 == 0 || i % 2 == 1) { //Todo fix this statement
+        } else {
+            break;
+        }
+    }
 }
 
 int main(){
     /*Read word from the keyboard using scanf*/
+    char myString[20];
+    scanf("%s", myString);
 
     /*Call sticky*/
+    sticky(myString);
 
     /*Print the new word*/
+    printf("%s", myString);
 
     return 0;
 }
